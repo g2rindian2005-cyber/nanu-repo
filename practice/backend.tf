1eccf7ed12d8706104566ecb5a5039d0a02ddc24
gokul <gokul9356782322@gmail.com>
2026-03-14 12:51:58 +0530

    all-vpc-conf

--- practice/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "config-bucket-739013795335"
-    key    = "jawan-folder/terraform.tf"
-    region = "us-east-1"
-    dynamodb_table = "gokul-lock-table"
-  }
-}
